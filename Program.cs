5a859a12682fec47f4f86210b6221c4c2c0b91ef
ModinaEN <modinaen@gmail.com>
2022-12-02 22:04:20 +0300

    Создана программа для нахождения наибольшего числа

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+Console.Write($"Введите число a: \n");
+int a = int.Parse(Console.ReadLine());
+Console.Write($"Введите число b: \n");
+int b = int.Parse(Console.ReadLine());
+
+if (a>b) {
+    Console.Write($"Число {a}");}
+else {
+   Console.Write($"Число {b} больше\n");}

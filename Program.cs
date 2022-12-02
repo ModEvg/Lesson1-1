7c5ab522b1206d9f2a9d75bc916c226c579f3735
ModinaEN <modinaen@gmail.com>
2022-12-02 22:49:38 +0300

    Создана программа для нахождения максимального из трех чисел

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿// Программа, которая на вход принимает два числа и выдает, какое число большее, а какое меньшее.
+﻿// Программа, которая на вход принимает три числа и выдает, какое число максимальное
 Console.Write($"Введите число a: \n");
 int a = int.Parse(Console.ReadLine());
 Console.Write($"Введите число b: \n");
 int b = int.Parse(Console.ReadLine());
+Console.Write($"Введите число c: \n");
+int c = int.Parse(Console.ReadLine());
 
-if (a>b) {
-    Console.Write($"Число {a}");}
-else {
-   Console.Write($"Число {b} больше\n");}
+if (a>b & a>c)
+    { Console.Write($"Число {a} максимальное");}
+else
+    if (b>c) 
+    {Console.Write($"Число {b} максимальное");}
+    else   {Console.Write($"Число {c} максимальное");}
